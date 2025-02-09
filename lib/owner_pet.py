27e15a48b1c19f10812fe900fdb75d33c329ef96
+ <+>
2025-02-09 13:36:15 +0300

    One-to-Many Object Relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception("Invalid pet type")
+        self.owner = owner
+        Pet.all.append(self)     
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Argument must be an instance of Pet")
+        else:
+            pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
